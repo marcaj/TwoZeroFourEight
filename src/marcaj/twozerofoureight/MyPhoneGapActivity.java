216e1c4d0f6e9153cfaf776d1d3d2c5e0c41a5c4
Marcaj <marcaj13@gmail.com>
2014-10-26 00:38:45 +0200

    Refactor: name main file as index.html

--- src/marcaj/twozerofoureight/MyPhoneGapActivity.java
+++ src/marcaj/twozerofoureight/MyPhoneGapActivity.java
@@ -7,6 +7,6 @@ public class MyPhoneGapActivity extends DroidGap {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		super.loadUrl("file:///android_asset/www/pages/login.html");
+		super.loadUrl("file:///android_asset/www/index.html");
 	}
 }
